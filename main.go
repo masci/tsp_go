8350686a435e680a9a1e8664050dca182230173d
Massimiliano Pippi <mpippi@gmail.com>
2015-06-03 18:20:51 +0200

    changed alltours implementation

--- main.go
+++ main.go
@@ -63,8 +63,7 @@ func (t *Tour) contains(city City) bool {
 
 // solve the tsp problem for the given array of City
 func alltours_tsp(cities []City) Tour {
-	var tours []Tour
-	alltours(&tours, cities, 0)
+	tours := alltours(cities)
 	return shortest_tour(tours)
 }
 
@@ -80,17 +79,32 @@ func shortest_tour(tours []Tour) Tour {
 }
 
 // Return all possible tours for the City instances contained in the array
-func alltours(tours *[]Tour, cities []City, index int) {
-	t := Tour{cities}
-	if index == len(cities)-1 {
-		*tours = append(*tours, t)
-	} else {
-		for j := index; j < len(t.cities); j++ {
-			t.swap(index, j)
-			alltours(tours, t.cities, index+1)
-			t.swap(index, j)
+// http://en.wikipedia.org/wiki/Heap%27s_algorithm
+func alltours(cities []City) []Tour {
+	var helper func(cities []City, index int)
+	res := []Tour{}
+
+	helper = func(cities []City, n int) {
+		tmp := make([]City, len(cities))
+		copy(tmp, cities)
+		tour := Tour{tmp}
+
+		if n == 1 {
+			res = append(res, tour)
+		} else {
+			for i := 0; i < n; i++ {
+				helper(tour.cities, n-1)
+				if n%2 == 1 {
+					tour.swap(i, n-1)
+				} else {
+					tour.swap(0, n-1)
+				}
+			}
 		}
 	}
+
+	helper(cities, len(cities))
+	return res
 }
 
 // Make a set of n cities, each with random coordinates within a (width x height) rectangle
@@ -107,5 +121,11 @@ func cities(n int, width int, height int, seed int64) []City {
 }
 
 func main() {
-	fmt.Printf("%+v\n", alltours_tsp(cities(3, 200, 100, 42)))
+	cs := cities(3, 200, 100, 42)
+	fmt.Println("Cities:")
+	for _, c := range cs {
+		fmt.Printf("%v\n", c)
+	}
+	fmt.Println("Best tour:")
+	fmt.Printf("%+v\n", alltours_tsp(cs))
 }
