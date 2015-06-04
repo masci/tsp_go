6e7b48e92ef395df2772a6e3bf3c93d7b4a4329b
Massimiliano Pippi <mpippi@gmail.com>
2015-06-04 13:08:00 +0200

    added improved algorithm

---
+++ improved_alltours_tsp_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestImprovedAlltours(t *testing.T) {
+	c := cities(4, 100, 100, 42)
+	tours := improved_alltours(c)
+
+	if len(tours) != 6 {
+		t.Error("Expected", 6, "found", len(tours))
+	}
+
+	for _, tour := range tours {
+		for _, c := range c {
+			if !tour.contains(c) {
+				t.Error("Expected", c, "in tour", tour, "but was not found")
+			}
+		}
+	}
+}
+
+func benchImprovedAlltoursTsp(i int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		improved_alltours_tsp(cities(i, 900, 600, 42))
+	}
+}
+
+func BenchmarkImprovedAlltoursTsp5(b *testing.B) {
+	benchImprovedAlltoursTsp(5, b)
+}
+
+func BenchmarkImprovedAlltoursTsp8(b *testing.B) {
+	benchImprovedAlltoursTsp(8, b)
+}
+
+func BenchmarkImprovedAlltoursTsp10(b *testing.B) {
+	benchImprovedAlltoursTsp(10, b)
+}
