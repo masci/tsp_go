1fd83f4714e2db44f51d49f054b87061801760da
Massimiliano Pippi <mpippi@gmail.com>
2015-06-04 12:16:19 +0200

    moved out types definition in another module

--- main.go
+++ main.go
@@ -1,80 +1,9 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"math"
 	"math/rand"
 )
-
-type City struct {
-	x, y float64
-}
-
-func (c *City) distance(other *City) (float64, error) {
-	if other.x < 0 || other.y < 0 {
-		return 0.0, errors.New("Distance only allows positive coordinates")
-	}
-	sx := math.Pow(other.x-c.x, 2)
-	sy := math.Pow(other.y-c.y, 2)
-	return math.Sqrt(sx + sy), nil
-}
-
-type Tour struct {
-	cities  []City
-	_length float64
-}
-
-// Tour constructor
-func NewTour(cities []City) *Tour {
-	t := Tour{cities: cities, _length: 0.0}
-	return &t
-}
-
-// swap two cities from a tour
-func (t *Tour) swap(i int, j int) {
-	tmp := t.cities[i]
-	t.cities[i] = t.cities[j]
-	t.cities[j] = tmp
-}
-
-// append a new City to this Tour
-func (t *Tour) append(c City) {
-	t.cities = append(t.cities, c)
-	t._length = 0.0 // length needs to be recomputed
-}
-
-// replace the array of cities
-func (t *Tour) setCities(cities []City) {
-	t.cities = cities
-	t._length = 0.0
-}
-
-// compute Tour length
-func (t *Tour) length() float64 {
-	if t._length == 0.0 {
-		for i, c := range t.cities {
-			if i > 0 {
-				d, _ := c.distance(&(t.cities[i-1]))
-				t._length += d
-			} else {
-				d, _ := c.distance(&(t.cities[len(t.cities)-1]))
-				t._length += d
-			}
-		}
-	}
-	return t._length
-}
-
-// does a Tour contain a City?
-func (t *Tour) contains(city City) bool {
-	for _, c := range t.cities {
-		if c == city {
-			return true
-		}
-	}
-	return false
-}
 
 // solve the tsp problem for the given array of City
 func alltours_tsp(cities []City) Tour {
